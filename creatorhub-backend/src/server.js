0959f0674252aa86a8ac4fe69d8547931749c38c
Sanyam Tyagi <sanyam.tyagi617@gmail.com>
2026-04-23 16:14:39 +0530

    updated code

--- creatorhub-backend/src/server.js
+++ creatorhub-backend/src/server.js
@@ -16,10 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.post("/test-login", (req, res) => {
-  res.send("TEST LOGIN WORKS");
-});
-console.log("AI ROUTE MOUNTED AT /api/ai");
+
 app.use("/api/auth",      require("./routes/authRoutes"));
 app.use("/api/platforms", require("./routes/platformroutes"));
 app.use("/api/analytics", require("./routes/analyticsroutes"));
